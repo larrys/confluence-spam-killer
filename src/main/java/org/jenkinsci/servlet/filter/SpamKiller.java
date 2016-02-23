159732cdbde5e5e927246ffdd4fd907fd6d88d2e
Larry Shatzer, Jr <larrys@gmail.com>
2016-02-23 07:33:01 -0700

    Filter the following actions: * page creation * page edits * comments being added

--- src/main/java/org/jenkinsci/servlet/filter/SpamKiller.java
+++ src/main/java/org/jenkinsci/servlet/filter/SpamKiller.java
@@ -1,8 +1,6 @@
 package org.jenkinsci.servlet.filter;
 
 import com.atlassian.plugin.servlet.PluginHttpRequestWrapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -15,53 +13,46 @@ import java.io.IOException;
 import java.util.regex.Pattern;
 
 public class SpamKiller implements Filter {
-    private static final Logger log = LoggerFactory.getLogger(SpamKiller.class);
     private final Pattern phoneNumberRegex = Pattern.compile(".*8(?:\\d{2})[ \\*~_\\-.=)]*\\d{3}[ \\*~_.\\-=]*\\d{4}.*");
+
 
     public void init(FilterConfig filterConfig) throws ServletException {}
 
     public void destroy() {}
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        String title = request.getParameter("title");
-        if(title != null) {
-            if (hasBannedWord(title, request.getParameter("wysiwygcontent"),
-                "printer support",
-                "q.u.i.c.k.b.o.o.k.s",
-                "quickbook",
-                "quicken",
-                "s.u.p.p.o.r.t.",
-                "Samsung Printer",
-                "solahartcenter.com",
-                "support phone",
-                "toll free",
-                "www.service-solahart.com"
-            )) {
-                HttpSession session = ((PluginHttpRequestWrapper) request).getSession();
-                String username = ((PluginHttpRequestWrapper) request).getSession().getAttribute("seraph_defaultauthenticator_user").toString();
-                log.error("Spam page detected. title={}, username={}", title, username);
-                session.invalidate();
-                throw new ServletException("This content has been determined to be spam.");
-            }
+        if (hasBannedWord(request.getParameter("title")) || hasBannedWord(request.getParameter("wysiwygcontent"))) {
+            HttpSession session = ((PluginHttpRequestWrapper) request).getSession();
+            session.invalidate();
+            throw new ServletException("This content has been determined to be spam.");
         }
         chain.doFilter(request, response);
     }
 
-    private boolean hasBannedWord(String title, String content, String... bannedWords) {
-        if(hasPhoneNumber(title, content)) {
-            return true;
-        }
-        for (String bannedWord : bannedWords) {
-            if (title != null && title.toLowerCase().contains(bannedWord) ||
-                content != null && content.toLowerCase().contains(bannedWord)) {
+    private boolean hasBannedWord(String content) {
+        String[] bannedWords = {
+            "printer support",
+            "q.u.i.c.k.b.o.o.k.s",
+            "quickbook",
+            "quicken",
+            "s.u.p.p.o.r.t.",
+            "Samsung Printer",
+            "solahartcenter.com",
+            "support phone",
+            "toll free",
+            "www.service-solahart.com"
+        };
+        if (content != null) {
+            if (phoneNumberRegex.matcher(content).matches()) {
                 return true;
+            }
+            for (String bannedWord : bannedWords) {
+                if (content.toLowerCase().contains(bannedWord)) {
+                    return true;
+                }
             }
         }
         return false;
-    }
-
-    private boolean hasPhoneNumber(String title, String content) {
-        return phoneNumberRegex.matcher(title).matches() || phoneNumberRegex.matcher(content).matches();
     }
 
 }
