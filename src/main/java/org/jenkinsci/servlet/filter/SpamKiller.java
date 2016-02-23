cee6bae923217acf81d64a48cce5dab2f3d356c2
Larry Shatzer, Jr <larrys@gmail.com>
2016-02-23 08:05:44 -0700

    Allow members of the moderators group to still post content that might be triggered as spam.
    If a person is a memeber of the spammers group, disallow ALL content from being created.

--- src/main/java/org/jenkinsci/servlet/filter/SpamKiller.java
+++ src/main/java/org/jenkinsci/servlet/filter/SpamKiller.java
@@ -1,6 +1,14 @@
 package org.jenkinsci.servlet.filter;
 
+import com.atlassian.confluence.user.AuthenticatedUserThreadLocal;
 import com.atlassian.plugin.servlet.PluginHttpRequestWrapper;
+import com.atlassian.spring.container.ContainerManager;
+import com.atlassian.user.EntityException;
+import com.atlassian.user.Group;
+import com.atlassian.user.GroupManager;
+import com.atlassian.user.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -8,25 +16,45 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.regex.Pattern;
 
 public class SpamKiller implements Filter {
-    private final Pattern phoneNumberRegex = Pattern.compile(".*8(?:\\d{2})[ \\*~_\\-.=)]*\\d{3}[ \\*~_.\\-=]*\\d{4}.*");
+    private static final Logger log = LoggerFactory.getLogger(SpamKiller.class);
 
+    private final Pattern phoneNumberRegex = Pattern.compile(".*8(?:\\d{2})[ \\*~_\\-.=)]*\\d{3}[ \\*~_.\\-=]*\\d{4}.*");
+    private final GroupManager groupManager = (GroupManager) ContainerManager.getComponent("groupManager");
 
     public void init(FilterConfig filterConfig) throws ServletException {}
 
     public void destroy() {}
 
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        if (hasBannedWord(request.getParameter("title")) || hasBannedWord(request.getParameter("wysiwygcontent"))) {
+        boolean isSpammer = false;
+        User confluenceUser = AuthenticatedUserThreadLocal.getUser();
+        try {
+            Group spammerGroup = null;
+            Group moderatorGroup = null;
+            moderatorGroup = groupManager.getGroup("moderators");
+            spammerGroup = groupManager.getGroup("spammer");
+            if (groupManager.hasMembership(spammerGroup, confluenceUser)) {
+                isSpammer = true;
+            } else if (hasBannedWord(request.getParameter("title")) || hasBannedWord(request.getParameter("wysiwygcontent"))) {
+                isSpammer = !groupManager.hasMembership(moderatorGroup, confluenceUser);
+            }
+        } catch (EntityException e) {
+            log.error("Entity Exception during spam check", e);
+        }
+        if (isSpammer) {
+            log.warn("Spammer detected: {}", confluenceUser.toString());
             HttpSession session = ((PluginHttpRequestWrapper) request).getSession();
             session.invalidate();
-            throw new ServletException("This content has been determined to be spam.");
+            ((HttpServletResponse) response).sendError(500, "Test");
+        } else {
+            chain.doFilter(request, response);
         }
-        chain.doFilter(request, response);
     }
 
     private boolean hasBannedWord(String content) {
